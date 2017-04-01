2fea76327169847321096596047e89bce6ba0fdb
jgreat <jgreat@jgreat.me>
2017-03-31 21:00:54 -0500

    boot.py

--- lcd_boot/boot.py
+++ lcd_boot/boot.py
@@ -24,6 +24,7 @@ lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 
 lcd.set_color(1.0, 0.0, 0.0)
 lcd.clear()
+lcd.home()
 lcd.message('''
 NG Security Systems
    REASON v0.0.1
