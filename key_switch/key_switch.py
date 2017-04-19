9e165190a7317d3dedbc34906526f7c7add4b41e
jgreat <jgreat@jgreat.me>
2017-04-18 20:17:18 -0500

    key_switch

--- key_switch/key_switch.py
+++ key_switch/key_switch.py
@@ -20,15 +20,44 @@ lcd_blue = 7  # Pin 7 is CE1
 lcd_columns = 20
 lcd_rows = 4
 
-boot_message = '''   REASON v1.0B7
- Ng Security Indst.
- ULTIMA RATIO REGUM
-'''
-
 lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                               lcd_d7, lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue)
 lcd.clear()
 lcd.set_backlight(0)
+
+from time import sleep
+
+
+def scroll(lcd, text, pause1=False, pause2=False, rep=False):
+    PAUSE_NEXT = 2
+    PAUSE_REP = 2
+    REPETITIONS = 4
+
+    if pause1:
+        PAUSE_NEXT = pause1
+    if pause2:
+        PAUSE_REP = pause2
+    if rep:
+        REPETITIONS = rep
+
+    n = 20
+    rows = [text[i:i + n] for i in range(0, len(text), n)]
+    n_rows = len(rows)
+    for i in range(REPETITIONS):
+        for x in range(n_rows):
+            lcd.home()
+            lcd.clear()
+            nxt = x + 1
+            lcd.message(rows[x] + "\n")
+            if nxt == n_rows:
+                sleep(2)
+
+                break
+            else:
+                lcd.message(rows[nxt])
+                sleep(PAUSE_REP)
+
+    lcd.clear()
 
 
 def on():
@@ -37,7 +66,17 @@ def on():
     lcd.set_color(1.0, 0.0, 0.0)
     lcd.clear()
     lcd.home()
-    lcd.message(boot_message.lstrip())
+    lcd.message('   REASON v1.0B7')
+    lcd.set_cursor(0, 1)
+    lcd.message(' Ng Security Indust.')
+    lcd.set_cursor(0, 2)
+    scroll(lcd, 'PRERELEASE VERSION -- NOT FOR FIELD USE -- DO NOT TEST IN A POPULATED AREA -- READY TO FIRE --')
+    lcd.set_cursor(0, 3)
+    while True:
+        lcd.message(' ULTIMA RATIO REGUM')
+        time.sleep(2)
+        lcd.set_cursor(0, 3)
+        lcd.message(' ' * 20)
 
 
 def off():
