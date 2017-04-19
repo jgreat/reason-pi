94e13b8aab3d6a146447906088e450c7b5e4aae7
jgreat <jgreat@jgreat.me>
2017-04-18 19:53:58 -0500

    key_switch

--- key_switch/key_switch.py
+++ key_switch/key_switch.py
@@ -20,26 +20,33 @@ lcd_blue = 7  # Pin 7 is CE1
 lcd_columns = 20
 lcd_rows = 4
 
+boot_message = '''
+   REASON v1.0B7
+ Ng Security Indst.
+ ULTIMA RATIO REGUM
+'''
+
 lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                               lcd_d7, lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue)
+lcd.enable_display(False)
 
 
 def on():
     print('Switch is On')
+    lcd.enable_display(True)
     lcd.set_color(1.0, 0.0, 0.0)
     lcd.clear()
     lcd.home()
-    msg = " NG Sec. Industries\n-REASON- v0.0.1"
-    lcd.message(msg)
+    lcd.message(boot_message.strip)
 
 
 def off():
     print('Switch is Off')
+    lcd.clear()
+    lcd.enable_display(False)
 
 
 key_switch = Button(pin=26)
-
 key_switch.when_pressed = on
 key_switch.when_released = off
-
 pause()
